8c68f4186eb5fc8b11fe792a65c35c95b05d0e31
codinglone <codinglone@gmail.com>
2023-05-28 18:38:24 +0400

    Added dotenv PORT and corrected configs

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
+require('dotenv').config()
 import { AppDataSource } from "./data-source"
+const PORT = process.env.PORT || 7000
 const fastify = require('fastify')({logger: true})
 
 const start = async () => {
     try {
-        await fastify.listen({ port: 5000 })
+        await fastify.listen({ port: PORT })
+        AppDataSource.initialize().then(async () => {
+
+            console.log("Connected!!!")
+        
+        }).catch(error => console.log(error))
     } catch (error) {
         fastify.log(error)
         process.exit(1)
     }
 }
 
-AppDataSource.initialize().then(async () => {
+start()
 
-    console.log("Connected!!!")
-
-}).catch(error => console.log(error))
