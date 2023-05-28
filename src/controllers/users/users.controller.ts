e4116bd3edef7690e3362d3a63b7a09331670d66
codinglone <codinglone@gmail.com>
2023-05-28 19:06:36 +0400

    Created createUserController function in users.controller.ts

---
+++ src/controllers/users/users.controller.ts
@@ -0,0 +1,12 @@
+import { UsersModel } from "../../models/users/users.model";
+
+const createUserController = async (req, reply) => {
+    try {
+        const response = await UsersModel.createUser(req.body)
+        reply.code(201).send(response)
+    } catch (error) {
+        reply.code(400).send(error)
+    }
+}
+
+export {createUserController}
