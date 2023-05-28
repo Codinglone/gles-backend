dc6928212525b93f48d3807eff147076effdc0fe
codinglone <codinglone@gmail.com>
2023-05-28 18:34:12 +0400

    added fastify bootstraping

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { AppDataSource } from "./data-source"
 const fastify = require('fastify')({logger: true})
 
+const start = async () => {
+    try {
+        await fastify.listen({ port: 5000 })
+    } catch (error) {
+        fastify.log(error)
+        process.exit(1)
+    }
+}
 
 AppDataSource.initialize().then(async () => {
 
