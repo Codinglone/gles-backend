5ddcda57e0cf6acf0e25e88fbc58370a3659e05c
codinglone <codinglone@gmail.com>
2023-05-28 18:44:14 +0400

    Added Swagger Documentation

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,18 @@ import { AppDataSource } from "./data-source"
 const PORT = process.env.PORT || 7000
 const fastify = require('fastify')({logger: true})
 
+fastify.register(require('@fastify/swagger'), {
+    exposeRoute: true,
+    routePrefix: '/api-docs',
+    swagger: {
+        info: {
+            title: 'GLES REST API Documentation',
+            description: 'GLES Web application backend using Fastify, TypeORM and Postgres',
+            version: '1.0.0'
+        }
+    }
+})
+
 const start = async () => {
     try {
         await fastify.listen({ port: PORT })
